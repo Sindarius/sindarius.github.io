525e92761bfcdbc6b7d8feeeed1b925bb9970693
Juan Rosario <pyroclasmx@yahoo.com>
2021-09-14 12:34:16 -0500

    Metatags

---
+++ precache-manifest.e0ecf7eca01da8d33559cbeab60ea6ae.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7209156cfabf0000bbd0e130087532ae",
+    "url": "/benchy_color.gcode"
+  },
+  {
+    "revision": "1130f744d12e9b7a8cc7",
+    "url": "/css/app.a52f33c9.css"
+  },
+  {
+    "revision": "6d26e5f022eed1ac6fd7",
+    "url": "/css/chunk-vendors.78264566.css"
+  },
+  {
+    "revision": "0ae8ba0711d7a9ac4edfee5f57a4ab3d",
+    "url": "/img/klipper.0ae8ba07.png"
+  },
+  {
+    "revision": "6aa2c67e99cf38bd6bd4514ced790f55",
+    "url": "/img/octoprint.6aa2c67e.png"
+  },
+  {
+    "revision": "291af9caa65208d800733940b17fa915",
+    "url": "/img/viewer.png"
+  },
+  {
+    "revision": "5b6b66c5d4186111abe396bf2ecb1745",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1130f744d12e9b7a8cc7",
+    "url": "/js/app.6b8d4835.js"
+  },
+  {
+    "revision": "6d26e5f022eed1ac6fd7",
+    "url": "/js/chunk-vendors.e2e38d8b.js"
+  },
+  {
+    "revision": "beeafb32667c60cf570a664317bcc5d5",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8ee2fc4313e4cc7965e2d1f8187dd8ee.js"
+  "/precache-manifest.e0ecf7eca01da8d33559cbeab60ea6ae.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "gcodeviewer_site"});
